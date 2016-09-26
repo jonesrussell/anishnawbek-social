e2e4df01d6599f3347c37279ee32e51286852ca6
jaapjan <mail@jaapjan.nl>
2016-09-26 12:10:55 +0100

    DS-2023 - Update.sh script for Docker containers.

--- dev-scripts/install/update.sh
+++ dev-scripts/install/update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Script to update Drupal in the docker container.
-cd /var/www/html/;
-
+cd /var/www/html/
+drush -y cr
 drush -y updatedb
+drush -y fra --bundle=social
+drush -y entup
