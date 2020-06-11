7b3dc2bb5359d43fc59ab078b55ec417bde8850a
Russell Jones <jonesrussell42@gmail.com>
2020-06-11 19:00:41 -0400

    WIP

---
+++ drush/nish.aliases.drushrc.php
@@ -0,0 +1,13 @@
+<?php
+
+$aliases['dev'] = array(
+  'uri' => 'https://dashboard-cms.ddev.site',
+  'root' => '/var/www/html/html',
+);
+
+$aliases['live'] = array(
+  'uri' => 'https://www.anishnawbek.xyz',
+  'remote-host' => 'www.anishnawbek.xyz',
+  'remote-user' => 'russell',
+  'root' => '/srv/www/www.anishnawbek.xyz/html',
+);
